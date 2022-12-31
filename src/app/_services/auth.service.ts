615ae2159906513530a8c634ab923e6a511b503a
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-31 18:04:09 -0400

    Updated: service with api url, httoptions and login, logout, signup functions

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8080/api/auth/';
+const httpOptions = {
+  headers: new HttpHeaders(
+    { 'Content-Type': 'application/json' }
+  )
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        username,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  register(fistname: string, lastname: string, username: string, email:string, homeaddress: string, phone: string, password: string): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        fistname,
+        lastname,
+        username,
+        email,
+        homeaddress,
+        phone,
+        password,
+      },
+      httpOptions
+    );
+  }
+
+  logout(): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signout',
+      {},
+      httpOptions
+    );
+  }
 }
