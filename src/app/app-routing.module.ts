5f87eefa71e7c6c5e518fd27cedaf5527170070a
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-01 18:22:57 -0400

    Updated: routes with home, register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: '', redirectTo: 'register', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
