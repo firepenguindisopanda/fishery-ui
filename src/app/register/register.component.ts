e26057bc9c6ca5ff98099b3f6fe5fb6806e018c5
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-01 18:30:32 -0400

    Updated: component with onSubmit fn

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 
 @Component({
@@ -7,5 +7,33 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  form: any = {
+    firstname: null,
+    lastname: null,
+    username: null,
+    email: null,
+    homeaddress: null,
+    phone: null,
+    password: null
+  };
+  isSuccessful: boolean = false;
+  isSignUpFailed: boolean = false;
+  errorMessage: string = '';
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void{}
 
+  onSubmit(): void{
+    const { firstname, lastname, username, email, homeaddress, phone, password } = this.form;
+    this.authService.register(firstname, lastname, username, email, homeaddress, phone, password).subscribe({
+      next: data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    });
+  }
 }
