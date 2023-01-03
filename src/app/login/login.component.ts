eb4d8d24f72fcb3dc7586aa2177e8a0140c136ba
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-02 22:04:02 -0400

    Updated: with onSubmit fn and reloadPage fn

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,6 +19,31 @@ export class LoginComponent implements OnInit{
   constructor(private authService: AuthService, private storageService: StorageService) { }
 
   ngOnInit(){
-    
+    if(this.storageService.isLoggedIn()){
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void{
+    const { username, password } = this.form;
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        //this.storageService.saveToken(data.accessToken);
+        this.storageService.saveUser(data);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.reloadPage();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void{
+    window.location.reload();
   }
 }
