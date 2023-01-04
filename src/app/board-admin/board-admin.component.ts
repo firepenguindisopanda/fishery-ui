df6541518a34991407b2cc6b16c1920fb5aba751
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-03 22:59:35 -0400

    Updated: with content variable and got data from UserService getAdminBoard fn

--- src/app/board-admin/board-admin.component.ts
+++ src/app/board-admin/board-admin.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
 
 @Component({
   selector: 'app-board-admin',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./board-admin.component.scss']
 })
 export class BoardAdminComponent {
+  content?: string;
+  constructor(private userService: UserService){}
 
+  ngOnInit(): void{
+    this.userService.getAdminBoard().subscribe({
+      next: data => {
+        this.content = data;
+      },
+      error: err => {
+        if(err.error){
+          try {
+            const res = JSON.parse(err.error);
+            this.content = res.message;
+          } catch (error) {
+            this.content = `Error with status: ${err.status} - ${err.statusText}`;
+          }
+        }else{
+          this.content = `Error with status: ${err.status}`;
+        }
+      }
+    });
+  }
 }
