91be34049d3667f5275bbf6208bf4c58e7da246c
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-03 23:10:41 -0400

    Updated: routes array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {
@@ -11,8 +16,23 @@ const routes: Routes = [
     path: 'register', component: RegisterComponent
   },
   {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'admin', component: BoardAdminComponent
+  },
+  {
+    path: 'mod', component: BoardModeratorComponent
+  },
+  {
+    path: 'user', component: BoardUserComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent
+  },
+  {
     path: '', redirectTo: 'register', pathMatch: 'full'
-  }
+  },
 ];
 
 @NgModule({
