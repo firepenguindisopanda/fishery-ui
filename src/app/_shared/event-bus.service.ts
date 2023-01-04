814c15248a32e6bd62f7a3e77df44a786d275dcb
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-03 23:24:09 -0400

    Updated: with an emit fn for the events and on fn

--- src/app/_shared/event-bus.service.ts
+++ src/app/_shared/event-bus.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { EventClass } from '../models/event-class.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventBusService {
+  private subject$ = new Subject<EventClass>();
 
   constructor() { }
+
+  emit(event: EventClass){
+    this.subject$.next(event);
+  }
+  on(eventName: string, action: any): Subscription{
+    return this.subject$.pipe(
+      filter((event: EventClass) => event.name === eventName),
+      map((event: EventClass) => event["value"])
+    ).subscribe(action);
+  }
 }
