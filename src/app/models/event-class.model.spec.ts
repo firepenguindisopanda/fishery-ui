4bf77511ebc4679e53ab318987c3086c752710ba
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-03 23:13:02 -0400

    Added: event class model

---
+++ src/app/models/event-class.model.spec.ts
@@ -0,0 +1,7 @@
+import { EventClass } from './event-class.model';
+
+describe('EventClass', () => {
+  it('should create an instance', () => {
+    expect(new EventClass()).toBeTruthy();
+  });
+});
