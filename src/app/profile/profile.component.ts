10f1bcf817b520a138d52c77cf0ca03d2fbf7885
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-03 22:13:03 -0400

    Updated: currentUser variable with value from getUser fn from storageService

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../_services/storage.service';
 
 @Component({
@@ -6,6 +6,11 @@ import { StorageService } from '../_services/storage.service';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  currentUser: string = '';
+  constructor(private storageService: StorageService){}
 
+  ngOnInit(): void{
+    this.currentUser = this.storageService.getUser();
+  }
 }
