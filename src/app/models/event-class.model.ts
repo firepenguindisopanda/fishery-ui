263b351238be9f4aab895fe2621f3dc0c68c36f3
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-03 23:16:33 -0400

    Updated: Event class with attributes and constructor

--- src/app/models/event-class.model.ts
+++ src/app/models/event-class.model.ts
@@ -1,2 +1,8 @@
 export class EventClass {
+    name: string = '';
+    value: any;
+    constructor(newname: string, newvalue: any){
+        this.name = newname;
+        this.value = newvalue;
+    }
 }
